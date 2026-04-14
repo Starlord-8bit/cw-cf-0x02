ac49750416127036c98737a665075a40cbb0ba25
Starlord-8bit <starlord-8bit@users.noreply.github.com>
2026-04-14 20:22:33 +0000

    refactor: align includes for cw-commons split

--- Clockface.h
+++ Clockface.h
@@ -16,8 +16,8 @@
 #include "DateI18nEN.h"
 
 // Commons
-#include "IClockface.h"
-#include "Icons.h"
+#include "widgets/clockface/IClockface.h"
+#include "display/Icons.h"
 
 #define CLOCKFACE_NAME "cw-cf-0x02"
 
